f11e88a8fa3ca82d4de94a328534571a53f432c3
guojian <2502585266@qq.com>
2023-11-08 17:58:34 +0800

    chore: test slot

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
 import UploadHugeFile from './components/uploadHugeFile.vue'
+import MockSlot from './components/mockSlot.vue'
 </script>
 
 <template>
   <UploadHugeFile />
+  <MockSlot title="测试 slot 插槽传值">
+    <template v-slot="mock">
+      <div class="mock">
+        <div class="slot">{{ mock.name }}</div>
+        <div class="slot">{{ mock.address }}</div>
+        <div class="slot">{{ mock.age }}</div>
+        <div class="slot">{{ mock.dribbble }}</div>
+        <div class="slot">{{ mock.email }}</div>
+        <div class="slot">{{ mock.medium }}</div>
+      </div>
+    </template>
+  </MockSlot>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
+<style scoped lang="less">
+.mock {
+  width: 100%;
+  height: 100%;
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  .slot {
+    font-size: 14px;
+    color: coral;
+  }
 }
 </style>
---
+++ src/components/mockSlot.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="parent">
+    <p class="title">{{ props.title || '标题' }}</p>
+    <slot v-bind="mocks"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  title?: string
+}>()
+
+const mocks = ref({
+  name: 'tom',
+  age: 18,
+  sex: '男',
+  address: '北京市朝阳区',
+  phone: '18888888888',
+  email: 'hzdkv@example.com',
+  qq: '123456789',
+  wechat: '123456789',
+  weibo: '123456789',
+  weixin: '123456789',
+  qqzone: '123456789',
+  github: '123456789',
+  linkedin: '123456789',
+  dribbble: '123456789',
+  instagram: '123456789',
+  medium: '123456789',
+  telegram: '123456789',
+  twitter: '123456789'
+})
+</script>
+
+<style scoped lang="less">
+.parent {
+  width: 100%;
+  height: 100%;
+
+  .title {
+    font-size: 20px;
+    font-weight: 600;
+    margin-bottom: 10px;
+  }
+}
+</style>
